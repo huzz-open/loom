fe6cc4ec741ca5f936d0a0cf0bc58d8d44d9df86
chenji1 <chenji1@kingsoft.com>
2025-01-22 11:01:44 +0800

    init

---
+++ core/src/main/java/io/huzz/loom/Main.java
@@ -0,0 +1,11 @@
+package io.huzz.loom;
+
+/**
+ * @author chenji
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
